3663a209f4d385846728c1b7e759442d35f3b7bc
Thorsten Beier <derthorstenbeier@gmail.com>
2022-03-22 14:11:02 +0100

    using py310 (#3)

--- build_all.sh
+++ build_all.sh
@@ -4,7 +4,7 @@ set -e
 mkdir -p bld_ems
 cd bld_ems
 
-emboa pack python core $HOME/micromamba/envs/emforgetest --version=3.11
+emboa pack python core $HOME/micromamba/envs/emforgetest --version=3.10
 
 
 
@@ -14,10 +14,7 @@ export CMAKE_SYSTEM_PREFIX_PATH=$PREFIX
 # export EMCC_FORCE_STDLIBS=1
 emcmake cmake  \
     -DCMAKE_FIND_ROOT_PATH_MODE_PACKAGE=ON \
-    -DCMAKE_VERBOSE_MAKEFILE=ON \
-    -DCMAKE_FIND_DEBUG_MODE=ON \
     -DCMAKE_PROJECT_INCLUDE=overwriteProp.cmake \
-    -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
     ..
 
 make -j12
@@ -26,5 +23,5 @@ emboa pack python pure-so \
     $HOME/micromamba/envs/emforgetest \
     embind11_side.so \
     embind11_side \
-    --version=3.11
+    --version=3.10
 
